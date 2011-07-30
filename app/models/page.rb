d842664afca58eb468474cbffcddb3ef49564ed2
Gabe Kessler <gabe@gabesstudio.com>
2011-07-30 17:27:26 -0400

    adding and styling file upload fields

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,6 @@
 class Page < ActiveRecord::Base
   has_friendly_id :heading, :use_slug => true, :sequence_separator => "_"
+  
+  has_attached_file :page_image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  
 end
--- app/models/slideshow_image.rb
+++ app/models/slideshow_image.rb
@@ -1,2 +1,5 @@
 class SlideshowImage < ActiveRecord::Base
+  
+  has_attached_file :slide, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  
 end
