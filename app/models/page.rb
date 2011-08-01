4d2ab591747d03868e30a0c2546aff6b6f90c293
Gabe Kessler <gabe@gabesstudio.com>
2011-07-31 22:41:37 -0400

    styling form errors

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,9 @@
 class Page < ActiveRecord::Base
-  has_friendly_id :title, :use_slug => true
   
   has_attached_file :page_image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  
+  validates_presence_of :title, :on => :create, :message => "can't be blank"
+  
+  has_friendly_id :title, :use_slug => true, :allow_nil => true
   
 end
